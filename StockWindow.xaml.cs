7c7a30427146764635c9c92d36c06cf26d841c7b
gablauria <gabi.cs.77@gmail.com>
2025-02-22 12:45:04 -0300

    v1 Final

---
+++ StockWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace TP2_Prod_Quimicos_GrupoL
+{
+    public partial class StockWindow : Window
+    {
+        public StockWindow(List<ProductoQuimico> productos)
+        {
+            InitializeComponent();
+            StockListBox.ItemsSource = productos;
+        }
+    }
+}
